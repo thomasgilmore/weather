474d144a297005917e56ad51e93fe360a8095fbf
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-04-18 15:11:09 -0400

    created Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./footer.css";
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            <p><a target="_blank" href="undefined/icons/set/partly-cloudy-day">Partly Cloudy Day icon</a> icon by <a target="_blank" href="">Icons8</a></p>
+        </div>
+    )
+}
