caae0c4637593332fbd3a367f0281f7e4e20ef27
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-04-18 15:11:56 -0400

    working on weather project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
 import Weather from "./components/Weather";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Weather />
+      <Footer />
     </div>
   );
 }
