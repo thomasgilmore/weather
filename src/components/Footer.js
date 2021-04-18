3a0132cebaf9f2cd04c1d7c3448e3a2aae50c9d3
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-04-18 18:51:02 -0400

    changed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,7 @@ export default function Footer() {
     return (
         <div className="footer">
             <p>
-                <a target="_blank" href="https://icons8.com/icons/set/partly-cloudy-day">Partly Cloudy Day icon</a> icon by <a target="_blank" href="https://icons8.com/">Icons8</a>
-            </p>
-            <p>
-            <a target="_blank" href="https://icons8.com/icons/set/umbrella-with-rain-drops">Umbrella With Rain Drops icon</a> icon by <a target="_blank" href="https://icons8.com/">Icons8</a>
+                <a target="_blank" href="https://icons8.com/icons/set/partly-cloudy-day">Partly Cloudy Day icon</a> and  <a target="_blank" href="https://icons8.com/icons/set/umbrella-with-rain-drops">Umbrella With Rain Drops icon</a> by <a target="_blank" href="https://icons8.com/">Icons8</a>
             </p>
             <p>
                 Photo by <a target="_blank" href="https://unsplash.com/@sindy_vonundzu_blitzdings?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Sindy Süßengut</a> on <a target="_blank" href="https://unsplash.com/s/photos/sky-background?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
