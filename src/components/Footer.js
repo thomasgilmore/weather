d07e8f0c40ed7316d6cdbdaa9eb97afeb56badbf
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-04-18 18:04:29 -0400

    changed some style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ export default function Footer() {
                 <a target="_blank" href="https://icons8.com/icons/set/partly-cloudy-day">Partly Cloudy Day icon</a> icon by <a target="_blank" href="https://icons8.com/">Icons8</a>
             </p>
             <p>
-                <a>Photo by <a target="_blank" href="https://unsplash.com/@sindy_vonundzu_blitzdings?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Sindy Süßengut</a> on <a target="_blank" href="https://unsplash.com/s/photos/sky-background?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></a>
+                Photo by <a target="_blank" href="https://unsplash.com/@sindy_vonundzu_blitzdings?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Sindy Süßengut</a> on <a target="_blank" href="https://unsplash.com/s/photos/sky-background?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
             </p>
         </div>
     )
