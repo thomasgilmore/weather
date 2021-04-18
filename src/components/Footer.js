9d21f401807ec9da4d884840c2491e4422567a77
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-04-18 16:41:56 -0400

    working on project

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,12 @@ import "./footer.css";
 export default function Footer() {
     return (
         <div className="footer">
-            <p><a target="_blank" href="undefined/icons/set/partly-cloudy-day">Partly Cloudy Day icon</a> icon by <a target="_blank" href="">Icons8</a></p>
+            <p>
+                <a target="_blank" href="https://icons8.com/icons/set/partly-cloudy-day">Partly Cloudy Day icon</a> icon by <a target="_blank" href="https://icons8.com/">Icons8</a>
+            </p>
+            <p>
+                <a>Photo by <a target="_blank" href="https://unsplash.com/@sindy_vonundzu_blitzdings?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Sindy Süßengut</a> on <a target="_blank" href="https://unsplash.com/s/photos/sky-background?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></a>
+            </p>
         </div>
     )
 }
