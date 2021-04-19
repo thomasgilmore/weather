731698786339eab3ca1abef422e12c6171db40bd
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-04-19 14:26:16 -0400

    removed unneeded code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,10 @@ export default function Footer() {
     return (
         <div className="footer">
             <p>
-                <a target="_blank" href="https://icons8.com/icons/set/partly-cloudy-day">Partly Cloudy Day icon</a> and  <a target="_blank" href="https://icons8.com/icons/set/umbrella-with-rain-drops">Umbrella With Rain Drops icon</a> by <a target="_blank" href="https://icons8.com/">Icons8</a>
+                <a target="_blank" href="https://icons8.com/icons/set/partly-cloudy-day" rel="noreferrer">Partly Cloudy Day icon</a> and  <a target="_blank" href="https://icons8.com/icons/set/umbrella-with-rain-drops" rel="noreferrer">Umbrella With Rain Drops icon</a> by <a target="_blank" href="https://icons8.com/" rel="noreferrer">Icons8</a>
             </p>
             <p>
-                Photo by <a target="_blank" href="https://unsplash.com/@sindy_vonundzu_blitzdings?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Sindy Süßengut</a> on <a target="_blank" href="https://unsplash.com/s/photos/sky-background?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                Photo by <a target="_blank" href="https://unsplash.com/@sindy_vonundzu_blitzdings?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" rel="noreferrer">Sindy Süßengut</a> on <a target="_blank" href="https://unsplash.com/s/photos/sky-background?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" rel="noreferrer">Unsplash</a>
             </p>
         </div>
     )
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-// import DisplayWeather from "./DisplayWeather";
 import "./weather.css";
 import { WeatherIcon } from './WeatherIcon';
 import moment from 'moment';
@@ -16,7 +15,6 @@ function Weather() {
 console.log(form);
 console.log(weather);
 
-  // const APIKEY = "Enter Your APIKEY here";
   async function weatherData(e) {
     e.preventDefault();
     if (form.zip === "") {
@@ -24,34 +22,25 @@ console.log(weather);
     } else {
       const data = await fetch(
         `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(form.zip)}.json?access_token=${process.env.REACT_APP_API_KEY_MAPBOX}&limit=1`
-        // `https://api.openweathermap.org/data/2.5/weather?q=${form.city},${form.country}&APPID=${APIKEY}`
       )
       .then((res) => res.json())
       .then((location) => {
-        console.log(location);
         const latitude = location.features[0].center[1];
         const longitude = location.features[0].center[0];
-        // console.log(latitude);
-        // console.log(longitude);
         const data2 = fetch(
           `http://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${process.env.REACT_APP_API_KEY_OPEN_WEATHER_MAP}&units=imperial`
         )
         .then((res2) => res2.json())
         .then((weatherData) => {
-          console.log(weatherData.daily);
           const days = weatherData.daily;
           
           let weatherCards = []
 
           days.forEach(day => {
-            console.log(day);
             const time = day.dt * 1000;
-            console.log(time);
             const dayOfWeek = moment(time).format('dddd');
-            console.log(dayOfWeek);
             const temp = Math.floor(day.temp.day);
             const rain = `${Math.floor(day.pop * 100)}%`;
-            console.log(rain)
             const weatherId = day.weather[0].id;
             const weatherIcon = day.weather[0].icon;
             const weatherImage = WeatherIcon(weatherId, weatherIcon);
@@ -64,10 +53,6 @@ console.log(weather);
           setWeather({ data: weatherCards })
         })
       })
-      //   .then((res) => res.json())
-      //   .then((data) => data);
-
-      // setWeather({ data: data });
     }
   }
 
@@ -99,7 +84,6 @@ console.log(weather);
     </div>
      
       <div className="weatherCardContainer">
-          {/* {console.log(weather)} */}
         {weather.data !== undefined ? (
           <div>
             {weather.data}
